837e5c2376317f8c5352b31c1b36e53e67ecd1f1
Selcus <90247816+Selcus@users.noreply.github.com>
2021-09-29 22:09:02 +0200

    COMMIT DECISIVO

--- ejerciciosPHP/Segunda_tanda/borrador.php
+++ ejerciciosPHP/Segunda_tanda/borrador.php
@@ -1,7 +1,24 @@
 <?php
-
+ 
     $ent1 = $_GET['entrada1'];
-    $ent1 = $_GET['entrada2'];
-    $ent1 = $_GET['entrada3'];
+    $ent2 = $_GET['entrada2'];
+    $ent3 = $_GET['entrada3'];
+ 
+    /*   TU CÓDIGO   */
+
+    /*   EJERCICIO 1   */
+
+    $ejer2 = $_GET["numero"];
+
+    if ($ejer2 =="v1") {
+        echo ("verdadero");
+    }
+    elseif ($ejer2=="v2"){
+        echo "falso";
+    }
+
+    /*   EJERCICIO 2   */
+
+    
 
 ?>
