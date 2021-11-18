b11505fdaf395ae25981448d256c96191f5a29a2
JuanmaCulsan <jcr0005@alu.medac.es>
2021-11-18 12:53:11 +0100

    para arriba

---
+++ javascriptCosas/ascensor/ascensor.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    var tamaño = parseInt(prompt("elija el tamaño del ascensor",""));
+
+    var pisos = document.createElement
+}
--- javascriptCosas/simonDice/simon.js
+++ javascriptCosas/simonDice/simon.js
@@ -1,6 +1,13 @@
 window.onload = function () {
     alert("Bienvenido a simon dice");
 
+    var celdas = document.querySelectorAll("td");
     var 
+    for (let i = 0; i < celdas.length; i++) {
+        celdas[i].onclick= function () {
+            
+        }
+        
+    }
 
 }
