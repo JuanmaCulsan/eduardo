b978fee12192816be4d762d2959e06f350bcb8dc
juanmanuel.culsan <juanmanuel.culsan@gmail.es>
2022-03-24 15:23:30 +0100

    ordenación de carpetas de javascriptcosas

--- javascriptCosas/clase/script.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var hola = "texto";
-
-var edu = "abuela";
---
+++ javascriptCosas/otros/simonDice/simon.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    alert("Bienvenido a simon dice");
+
+    var celdas = document.querySelectorAll("td");
+    for (let i = 0; i < celdas.length; i++) {
+        celdas[i].onclick= function () {
+            
+        }
+        
+    }
+
+}
